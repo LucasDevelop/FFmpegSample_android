1143958f28c44c55b294227762cf8d5ace01f320
lucas <lucas_developer@163.com>
2023-01-20 01:27:07 +0800

    111

--- AVcpp/src/main/cpp/src/Jni_util.cpp
+++ AVcpp/src/main/cpp/src/Jni_util.cpp
@@ -1,11 +1,24 @@
+#include <malloc.h>
+#include <cstring>
 #include "Jni_util.hpp"
 
+using namespace std;
 
 string string2str(JNIEnv *env,jstring jstr){
     //使用反射String 转换
+    char * c= nullptr;
     jclass clazz = env->FindClass("java/lang/String");
     jstring strencode = env->NewStringUTF("utf-8");
     jmethodID methodId =  env->GetMethodID(clazz,"getBytes","(Ljava/lang/String;)[B");
     jbyteArray byteArray= (jbyteArray)env->CallObjectMethod(jstr,methodId,strencode);
-
+    jsize alen = env->GetArrayLength(byteArray);
+    jbyte *ba = env->GetByteArrayElements(byteArray,JNI_FALSE);
+    if (alen>0){
+        c = static_cast<char *>(malloc(alen + 1));
+        memcpy(c,ba,alen);
+        c[alen]=0;
+    }
+    env->ReleaseByteArrayElements(byteArray,ba,0);
+    string str = c;
+    return str;
 }
